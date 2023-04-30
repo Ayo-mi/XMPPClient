2eaa532db490c31fc0f2e9344680eb40636db86c
Ayo-mi <ajayijubrilayo@gmail.com>
2023-04-30 22:58:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using XmppDotNet;
+using XmppDotNet.Xmpp.Register;
+
+Console.WriteLine("Hello, World!");
+public class RegisterAccountHandler : IRegister
+{
+    private XmppClient xmppClient;
+    public RegisterAccountHandler(XmppClient xmppClient)
+    {
+        this.xmppClient = xmppClient;
+    }
+
+    public bool RegisterNewAccount => true;
+
+    public async Task<Register> RegisterAsync(Register register)
+    {
+        return await Task<Register>.Run(() =>
+        {
+            register.RemoveAll();
+            register.Username = xmppClient.Jid;
+            register.Password = xmppClient.Password;
+
+            return register;
+        });
+    }
+
+}
